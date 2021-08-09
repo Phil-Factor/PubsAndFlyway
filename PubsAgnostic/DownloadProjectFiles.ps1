e78a66a3e5a274a487f8855b69ec3db8e1594673
Phil Factor <philipFactor@gmail.com>
2021-08-09 11:53:58 +0100

    Create DownloadProjectFiles.ps1

---
+++ PubsAgnostic/DownloadProjectFiles.ps1
@@ -0,0 +1,8 @@
+﻿$ProjectFolder="$env:Temp\PubsAndFlyway\PubsAgnostic";
+$Params = @{
+    'Owner' = 'Phil-Factor';
+    'Repository' = 'PubsAndFlyway';
+    'RepoPath' = 'PubsAgnostic';
+    'DestinationPath' = $ProjectFolder
+}
+Get-FilesFromRepo @Params
