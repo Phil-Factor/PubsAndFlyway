cbfef3f1f5fdc1c027a42570795de5ff15eb57a5
Phil Factor <philipFactor@gmail.com>
2021-09-23 12:23:08 +0100

    Delete Working Copy of afterMigrate__Echo.bat

--- PubsFlywaySecondMigration/Scripts/Working Copy of afterMigrate__Echo.bat
+++
@@ -1,9 +0,0 @@
-rem need to be able to report errors - must not have unhandled errors
-rem Need to discover version
-rem must have credentials
-
-
-set >c:\users\andre\documents\parameters.txt
-FP__projectName__
-FP__projectDescription__
-	
