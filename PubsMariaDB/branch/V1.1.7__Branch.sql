c8b0ca8fc6e362bae58bd3bc6a12682a379c96e4
Phil Factor <philipFactor@gmail.com>
2021-10-14 19:09:00 +0100

    minor tweaks

--- PubsMariaDB/branch/V1.1.7__Branch.sql
+++ PubsMariaDB/branch/V1.1.7__Branch.sql
@@ -1,17 +1,3 @@
--- --------------------------------------------------------
--- Host:                         PentlowMillServ
--- Server version:               10.6.3-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             11.3.0.6295
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 
 -- Dumping database structure for dbo
 CREATE DATABASE IF NOT EXISTS `dbo` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
