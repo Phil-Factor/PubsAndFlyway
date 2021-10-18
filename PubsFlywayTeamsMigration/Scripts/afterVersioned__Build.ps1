95d076b885c3847e397100badb3e887131c40330
Phil Factor <philipFactor@gmail.com>
2021-10-18 12:01:31 +0100

    modifications to allow use of placeholders

--- PubsFlywayTeamsMigration/Scripts/afterVersioned__Build.ps1
+++ PubsFlywayTeamsMigration/Scripts/afterVersioned__Build.ps1
@@ -1,20 +1,36 @@
-﻿#run the library script, assuming it is in the project directory containing the script directory
+﻿<#To set off any task, all you need is a PowerShell script that is created in such a way that it can be
+executed by Flyway when it finishes a migration run. Although you can choose any of the significant points
+in any Flyway action, there are only one or two of these callback points that are useful to us.  
+This can be a problem if you have several chores that need to be done in the same callback or you have a
+stack of scripts all on the same callback, each having to gather up and process parameters, or pass 
+parameters such as the current version from one to another. 
+
+A callback script can’t be debugged as easily as an ordinary script. In this design, the actual callback 
+just executes a list of tasks in order, and you simply add a task to the list after you’ve debugged 
+and tested it & placed in the DatabaseBuildAndMigrateTasks.ps1 file.
+with just one callback script
+
+Each task is passed a standard ‘parameters’ object. This keeps the ‘complexity beast’ snarling in its lair.
+The parameter object is passed by reference so each task can add value to the data in the object, 
+such as passwords, version number, errors, warnings and log entries. 
+
+All parameters are passed by Flyway. It does so by environment variables that are visible to the script.
+You can access these directly, and this is probably best for tasks that require special information
+passed by custom placeholders, such as the version of the RDBMS, or the current variant of the version 
+you're building
+
+
+run the library script, assuming it is in the project directory containing the script directory #>
 . "..\DatabaseBuildAndMigrateTasks.ps1"
 
-<#
-$env:FP__errorLogFile__='TheErrorlog'
-$env:FP__reportLogFile__='TheReportLog'
-$env:FP__projectName__='TheProjectName'
-$env:FP__projectFolder__='TheProjectFolder'
-$env:FLYWAY_USER='Fred'
-$env:FLYWAY_URL='jdbc:sqlserver://MyServer:1433;maxResultBuffer=-1;sendTemporalDataTypesAsStringForBulkCopy=true;delayLoadingLobs=true;useFmtOnly=false;useBulkCopyForBatchInsert=false;cancelQueryTimeout=-1;sslProtocol=TLS;jaasConfigurationName=SQLJDBCDriver;statementPoolingCacheSize=0;serverPreparedStatementDiscardThreshold=10;enablePrepareOnFirstPreparedStatementCall=false;fips=false;socketTimeout=0;authentication=NotSpecified;authenticationScheme=nativeAuthentication;xopenStates=false;sendTimeAsDatetime=true;trustStoreType=JKS;trustServerCertificate=false;TransparentNetworkIPResolution=true;serverNameAsACE=false;sendStringParametersAsUnicode=true;selectMethod=direct;responseBuffering=adaptive;queryTimeout=-1;packetSize=8000;multiSubnetFailover=false;loginTimeout=15;lockTimeout=-1;lastUpdateCount=true;encrypt=false;disableStatementPooling=true;databaseName=PubsTwo;columnEncryptionSetting=Disabled;applicationName=Microsoft JDBC Driver for SQL Server;applicationIntent=readwrite;'
-#>
-#our regex for gathering variables from Flyway's URL
+<# The most useful data passed to this script by Flyway is the URL that you used to call Flyway. This
+is likely to tell you the server, port, database and the type of database (RDBMS). We can use the URL
+if we just want to make JDBC calls. We can't and don't. Instead we extract the connection details
+and use these. #>
 $FlywayURLRegex =
 'jdbc:(?<RDBMS>[\w]{1,20})://(?<server>[\w]{1,20}):?(?<port>[\d]{1,4}|);.+databaseName=(?<database>[\w]{1,20})'
 
-#this FLYWAY_URL contains the current database, port and server so
-# it is worth grabbing
+#this FLYWAY_URL contains the current database, port and server so it is worth grabbing
 $ConnectionInfo = $env:FLYWAY_URL #get the environment variable
 if ($ConnectionInfo -eq $null) #OMG... it isn't there for some reason
 { Write-error 'missing value for flyway url' }
@@ -31,6 +47,7 @@ $DatabaseDetails = @{
 	'ProjectFolder' = ''; #where all the migration files are
     'project' = ''; #the name of your project
     'projectDescription'=''; #a brief description of the project
+    'flywayTable'='';#The name and schema of the flyway Table
 	'uid' = ''; #optional if you are using windows authewntication
 	'pwd' = ''; #only if you use a uid. Leave blank. we fill it in for you
 	'locations' = @{ }; # for reporting file locations used
@@ -47,44 +64,53 @@ if ($ConnectionInfo -imatch $FlywayURLRegex)
 }
 else
 { write-error "failed to obtain the value of the RDBMS, server, Port or database from the FLYWAY_URL" }
+
 $DatabaseDetails.uid = $env:FLYWAY_USER;
 $DatabaseDetails.Project = $env:FP__projectName__;
 $DatabaseDetails.ProjectDescription = $env:FP__projectDescription__;
 $DatabaseDetails.ProjectFolder = split-path $PWD.Path -Parent;
-
+if ($env:FP__flyway_defaultSchema__ -ne $null -and $env:FP__flyway_table__ -ne $null)
+    {$DatabaseDetails.flywayTable="$($env:FP__flyway_defaultSchema__).$($env:FP__flyway_table__)"}
+    else
+    {$DatabaseDetails.flywayTable='dbo.flyway_schema_history'};
 <#
-this assumes that reports will go in "$($env:USERPROFILE)\Documents\GitHub\$(
-		$param1.EscapedProject)\$($param1.Version)\Reports" but will return
+You can dump this array for debugging so that it is displayed by Flyway
+
+$DatabaseDetails|convertTo-json
+
+these routines write to  reports  in "$($env:USERPROFILE)\Documents\GitHub\$(
+		$param1.EscapedProject)\$($param1.Version)\Reports" and will return
         the path in the $DatabaseDetails if you need it. Set it to whatever
-        you want
+        you want in the file DatabaseBuildAndMigrateTasks.ps1
 You will also need to set SQLCMD to the correct value. This is set by a string
 $SQLCmdAlias in ..\DatabaseBuildAndMigrateTasks.ps1
 
+below are the tasks you want to execute. Some, like the on getting credentials, are essential befor you
+execute others
 in order to execute tasks, you just load them up in the order you want. It is like loading a 
 revolver. 
 #>
-$PostMigrationInvocations = @(
+$PostMigrationTasks = @(
 	$FetchAnyRequiredPasswords, #checks the hash table to see if there is a username without a password.
     #if so, it fetches the password from store or asks you for the password if it is a new connection
 	$GetCurrentVersion, #checks the database and gets the current version number
     #it does this by reading the Flyway schema history table. 
-<#	$CreateBuildScriptIfNecessary, #writes out a build script if there isn't one for this version. This
+#	$CreateBuildScriptIfNecessary, #writes out a build script if there isn't one for this version. This
     #uses SQL Compare
 	$CreateScriptFoldersIfNecessary, #writes out a source folder with an object level script if absent.
     #this uses SQL Compare
-	$ExecuteTableSmellReport, #checks for table-smells
+#	$ExecuteTableSmellReport, #checks for table-smells
     #This is an example of generating a SQL-based report
-	$ExecuteTableDocumentationReport, #publishes table docuentation as a json file that allows you to
+#	$ExecuteTableDocumentationReport, #publishes table docuentation as a json file that allows you to
     #fill in missing documentation. 
-	$CheckCodeInDatabase, #does a code analysis of the code in the live database in its current version
+#	$CheckCodeInDatabase, #does a code analysis of the code in the live database in its current version
     #This uses SQL Codeguard to do this
-	$CheckCodeInMigrationFiles, #does a code analysis of the code in the migration script
+#	$CheckCodeInMigrationFiles, #does a code analysis of the code in the migration script
     #This uses SQL Codeguard to do this
-	$IsDatabaseIdenticalToSource, # uses SQL Compare to check that a version of a database is correct
+#	$IsDatabaseIdenticalToSource, # uses SQL Compare to check that a version of a database is correct
     #this makes sure that the target is at the version you think it is.
-    $SaveDatabaseModelIfNecessary #writes out the database model
-    #This writes out a model of the version for purposes of comparison, narrative and checking. #>
+#    $SaveDatabaseModelIfNecessary #writes out the database model
+    #This writes out a model of the version for purposes of comparison, narrative and checking. 
     $CreateUndoScriptIfNecessary
     )
-Process-FlywayTasks $DatabaseDetails $PostMigrationInvocations
-
+Process-FlywayTasks $DatabaseDetails $PostMigrationTasks
--- PubsFlywayTeamsMigration/UNDOBuild.ps1
+++ PubsFlywayTeamsMigration/UNDOBuild.ps1
@@ -1,29 +1,43 @@
-﻿<# tell PowerShell where Flyway is. You need to change that to the correct path for your installation.
-You only need to do this if your flyway installation has not provideded a path cvommand #>
-Set-Alias Flyway  'C:\ProgramData\chocolatey\lib\flyway.commandline\tools\flyway-7.15.0\flyway.cmd' -Scope local
-#specify the DSN, and create the ODVC connection
+﻿Set-Alias Flyway  'C:\ProgramData\chocolatey\lib\flyway.commandline\tools\flyway-8.0.1\flyway.cmd' -Scope local
 
-# and here are our project details. The project folder
-$ProjectFolder = '<MyPathTo>\PubsAndFlyway\PubsFlywayTeamsMigration'
-$Server = '<MyUserName>'
-$Database = '<MyDatabaseName>';
-$ProjectName = 'Publications';
-$ProjectDescription = 'A sample project to demonstrate Flyway Teams, using the old Pubs database'
-<# you only need this username and password if there is no domain authentication #>
-$username = '<MyUserName>'
-$port = '<MyPortNumber>'
+<# tell PowerShell where Flyway is, and the version that you want to run. You need to change that to the correct 
+path for your installation.
+You only need to do this if your flyway installation has not provideded a path command #>
+
+# and here are our project details. 
+
+$Details = @{
+	'server' = '<MyServer>'; #The Server name
+	'database' = '<MyDatabaseName>'; #The Database
+	'Port' = '<portNumber>'; #The Database
+	'uid' = '<MyUserName>'; #The User ID. you only need this if there is no domain authentication or secrets store #>
+	'pwd' = ''; # The password. This gets filled in if you request it
+	'version' = ''; # TheCurrent Version. This gets filled in if you request it
+	'schemas' = '<list,of,schemas,You''re,Using';
+	'project' = '<NameOfTheProject>'; #Just the simple name of the project
+	'ProjectDescription' = '<simple explanation of the database being built, for posterity>'
+	'ProjectFolder' = '<MyPathTo>\PubsAndFlyway\PubsFlywayTeamsMigration'; #parent the scripts directory
+	'Warnings' = @{ }; # Just leave this be. Filled in for your information                                                                                                                                                                                                          
+	'Problems' = @{ }; # Just leave this be. Filled in for your information                                                                                                                                                                                                           
+	'Locations' = @{ }; # Just leave this be. Filled in for your information
+}
 
 # add a bit of error-checking. Is the project directory there
-if (-not (Test-Path "$ProjectFolder"))
+if (-not (Test-Path "$($Details.ProjectFolder)"))
 { Write-Error "Sorry, but I couldn't find a project directory at the $ProjectFolder location" }
 # ...and is the script directory there?
-if (-not (Test-Path "$ProjectFolder\Scripts"))
+if (-not (Test-Path "$($Details.ProjectFolder)\Scripts"))
 { Write-Error "Sorry, but I couldn't find a scripts directory at the $ProjectFolder\Scripts location" }
 # now we get the password if necessary
-if ($username -ne '') #then it is using SQL Server Credentials
+if ($Details.server -eq $null -or $Details.server -eq '<MyServer>')
+{ Write-Error "Sorry, but you really do need to put the name of your server in the details" }
+if ($Details.database -eq $null -or $Details.database -eq '<MyDatabaseName>')
+{ Write-Error "Sorry, but we really need to know the database" }
+
+if (!([string]::IsNullOrEmpty($Details.uid))) #then it is using SQL Server Credentials
 {
 	# we see if we've got these stored already
-	$SqlEncryptedPasswordFile = "$env:USERPROFILE\$($username)-$Server-sqlserver.xml"
+	$SqlEncryptedPasswordFile = "$env:USERPROFILE\$($Details.uid)-$($Details.server)-sqlserver.xml"
 	# test to see if we know about the password in a secure string stored in the user area
 	if (Test-Path -path $SqlEncryptedPasswordFile -PathType leaf)
 	{
@@ -34,90 +48,49 @@ if ($username -ne '') #then it is using SQL Server Credentials
 	else #then we have to ask the user for it (once only)
 	{
 		# hasn't got this set for this login
-		$SqlCredentials = get-credential -Credential $UserName
+		$SqlCredentials = get-credential -Credential $Details.uid
 		# Save in the user area 
 		$SqlCredentials | Export-CliXml -Path $SqlEncryptedPasswordFile
         <# Export-Clixml only exports encrypted credentials on Windows.
         otherwise it just offers some obfuscation but does not provide encryption. #>
 	}
 	$FlywayUndoArgs =
-	@("-url=jdbc:sqlserver://$($Server):$port;databaseName=$Database",
-		"-locations=filesystem:$ProjectFolder\Scripts", <# the migration folder #>
+	@("-url=jdbc:sqlserver://$($Details.Server)$(if([string]::IsNullOrEmpty($Details.port)){''}else{':'+$Details.port});databaseName=$($Details.database)",
+		"-locations=filesystem:$($Details.projectFolder)\Scripts", <# the migration folder #>
 		"-user=$($SqlCredentials.UserName)",
 		"-password=$($SqlCredentials.GetNetworkCredential().password)")
-	$SQLCmdArgs = @{
-		'Server' = $Server; 'Database' = $Database; 'User' = $SqlCredentials.UserName;
-		'Password' = $SqlCredentials.GetNetworkCredential().password
-	}
+    $Details.pwd=$SqlCredentials.GetNetworkCredential().password;
+    $Details.uid=$SqlCredentials.UserName
 }
 else
 {
 	$FlywayUndoArgs =
 	@("-url=jdbc:sqlserver://$($Server):$port;databaseName=$Database;integratedSecurity=true".
 		"-locations=filesystem:$ProjectFolder\Scripts")<# the migration folder #>
-	$SQLCmdArgs = @{ 'Server' = $Server; 'Database' = $Database }
 }
 $FlywayUndoArgs += <# the project variables that we reference with placeholders #>
 @(
-	"-schemas=dbo,Classic,people",
-	"-placeholders.schemas=dbo,Classic,people", #This isn't passed to callbacks otherwise
-	"-placeholders.projectDescription=$ProjectDescription",
-	"-placeholders.projectName=$ProjectName")
+	"-schemas=$($Details.schemas)",
+	"-placeholders.schemas=$($Details.schemas)", #This isn't passed to callbacks otherwise
+	"-placeholders.projectDescription=$($Details.ProjectDescription)",
+	"-placeholders.projectName=$($Details.Project)")
 
-<# if your install of the SQL Server utilities has added the environment PATH variable then
-you don't need this #>
-$SQLCmdAlias = "$($env:ProgramFiles)\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\sqlcmd.exe"
-Set-Alias SQLCmd $SQLCmdAlias
-<# we'll use a script block to access SQLCMD #>
 
-$GetdataFromSQLCMD = {<# a way of accessing SQL Server to get JSON results withhout having to open a
-connection. We are going to access the server via flyway and so only want one connection going #>
-	Param ($Theargs,
-		$query)
-	$TempFile = "$($env:Temp)\Temp.json"
-	$FullQuery = "Set Nocount On   $query"
-	if ($TheArgs.User -ne $null)
-	{
-		sqlcmd -S $TheArgs.server -d $TheArgs.database -Q "`"$FullQuery`"" -U $TheArgs.User -P $TheArgs.password -o `"$TempFile`" -u -y0
-	}
-	else
-	{
-		sqlcmd -S $TheArgs.server -d $TheArgs.database -Q "`"$FullQuery`"" -o `"$TempFile`" -u -y0
-	}
-	If (Test-Path -Path $TempFile)
-	{
-		$response = [IO.File]::ReadAllText($TempFile);
-		#Remove-Item $TempFile
-		if ($response -like 'Msg*')
-		{ write-error "$($TheArgs.database) says $Response" }
-		elseif ($response -like 'SqlCmd*')
-		{ write-error "SQLCMD says $Response" }
-		elseif ($response -like 'NULL*')
-		{ '' }
-		else
-		{ $response }
-	}
+@('1.1.1','1.1.2','1.1.3','1.1.4',
+'1.1.5','1.1.6','1.1.7','1.1.8',
+'1.1.9','1.1.10','1.1.11')| foreach{
+Flyway @FlywayUndoArgs migrate "-target=$_"
 }
-<# now we use the scriptblock to determine the version number and name from SQL Server
-#>
-$ServerVersion = $GetdataFromSQLCMD.Invoke($SQLCmdArgs, "
- Select  Cast(ParseName ( Cast(ServerProperty ('productversion') AS VARCHAR(20)), 4) AS INT)")
 
-[int]$VersionNumber = $ServerVersion[0]
-
-if ($VersionNumber -ne $null)
-{
-	$FlywayUndoArgs +=
-	@("-placeholders.canDoJSON=$(if ($VersionNumber -ge 13) { 'true' }
-			else { 'false' })",
-		"-placeholders.canDoStringAgg=$(if ($VersionNumber -ge 14) { 'true' }
-			else { 'false' })")
-}
-cd "$ProjectFolder\scripts"
 
 #if this works we're probably good to go
 
-Flyway @FlywayUndoArgs info 
-Flyway @FlywayUndoArgs clean 
-Flyway @FlywayUndoArgs migrate '-target=1.1.9'
-Flyway @FlywayUndoArgs undo '-target=1.1.3'
+Flyway @FlywayUndoArgs info -teams
+Flyway @FlywayUndoArgs clean -teams
+Flyway @FlywayUndoArgs migrate 
+Flyway @FlywayUndoArgs undo 
+#Flyway @FlywayUndoArgs migrate '-target=1.1.8'
+#Flyway @FlywayUndoArgs undo '-target=1.1.7'
+
+
+
